7c4a3c63abcf2a5f431006c4cacfc4322447836d
hibou <namnguyen107@gmail.com>
2018-04-23 00:09:03 +0200

    with scpi

--- simulation.py
+++ simulation.py
@@ -38,17 +38,18 @@ def compute_monthly_rate(year_rate):
     return math.pow(1 + year_rate, 1 / 12) - 1
 
 
-def compute_monthly_investment(monthly_saving, monthly_rate, nb_months):
+def compute_monthly_investment(monthly_saving, monthly_rate, nb_months, tax_monthly):
     result = 0.0
     for i in range(nb_months):
-        result = result * monthly_rate + monthly_saving
+        result = result * (1 + monthly_rate) + monthly_saving - tax_monthly
     return result
 
 
-def simulate():
+def simulate_buy():
     monthly_saving = 3000
-    current_saving = 50000
+    total_saving = 80000
     apport = 30000
+    current_saving = total_saving - apport
     apartment_price = 270000
     notaire_fee = apartment_price * 0.08
     nb_months = 12 * 15
@@ -56,20 +57,68 @@ def simulate():
     market_yield = 0.05
     taux_teg = 0.02
     inflation = 0.01
+    taxe_fonciere = 3000
+    taxe_fonciere_monthly = taxe_fonciere / 12
+    charge_coproprietaire = 4000
+    charge_coproprietaire_monthly = charge_coproprietaire / 12
+    frais_agence = 0.05
+
     mensualite = compute_mensualite(apartment_price + notaire_fee - apport, taux_teg, nb_months)
-    print("Mensualite: ", mensualite)
+
     investir_par_mois = monthly_saving - mensualite
     monthly_market_yield = compute_monthly_rate(market_yield)
-    all_monthly_investment = compute_monthly_investment(investir_par_mois, monthly_market_yield, nb_months)
+    all_monthly_investment = compute_monthly_investment(investir_par_mois, monthly_market_yield, nb_months,
+                                                        taxe_fonciere_monthly + charge_coproprietaire_monthly)
+
     from_original_investment = current_saving * math.pow(1 + market_yield, nb_year)
     all_investment = all_monthly_investment + from_original_investment
-    assets = (apartment_price + apport) * math.pow(1 + inflation, nb_year)
+    assets = apartment_price * math.pow(1 + inflation, nb_year) * (1 - frais_agence)
     return assets + all_investment
 
 
+def simulate_rent():
+    monthly_saving = 3000
+    rent = 1200
+    total_saving = 80000
+    nb_months = 12 * 15
+    nb_year = nb_months / 12
+    market_yield = 0.05
+    monthly_market_yield = compute_monthly_rate(market_yield)
+    all_monthly_investment = compute_monthly_investment(monthly_saving - rent, monthly_market_yield, nb_months, 0.0)
+    from_original_investment = total_saving * math.pow(1 + market_yield, nb_year)
+    return all_monthly_investment + from_original_investment
+
+
+def simulate_scpi():
+    monthly_saving = 3000
+    rent = 1150
+    total_saving = 80000
+    scpi_total = 100000
+    apport = 10000
+    nb_months = 12 * 15
+    nb_year = nb_months / 12
+    taux_teg = 0.028
+    mensualite = compute_mensualite(scpi_total - apport, taux_teg, nb_months)
+    market_yield = 0.05
+    frais_revente = 0.12
+    scpi_yield_annual = 0.06
+    rent_monthly = scpi_total * scpi_yield_annual / 12
+    monthly_market_yield = compute_monthly_rate(market_yield)
+    all_monthly_investment = compute_monthly_investment(monthly_saving - rent - mensualite + rent_monthly,
+                                                        monthly_market_yield, nb_months, 0.0)
+    inflation = 0.01
+    asset = scpi_total * math.pow(1 + inflation, nb_year) * (1 - frais_revente)
+
+    from_original_investment = (total_saving - apport) * math.pow(1 + market_yield, nb_year)
+
+    return all_monthly_investment + asset + from_original_investment
+
+
 if __name__ == "__main__":
-    # print(compute_mensualite(200000, 0.045, 240))
-    print(simulate())
+    print(compute_mensualite(100000, 0.028, 15*12))
+    print(simulate_buy())
+    print(simulate_rent())
+    print(simulate_scpi())
     # print(revenu(45609, 2.5))
     # plot()
 
